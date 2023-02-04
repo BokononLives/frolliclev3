242815fa822f59c167d610caf3a2afdccad42987
Jacob <jacob@ezlandlordforms.com>
2023-02-04 09:14:12 -0500

    Use player input manager in grid scene

--- Assets/Scripts/PieceGenerator.cs
+++ Assets/Scripts/PieceGenerator.cs
@@ -1,12 +1,16 @@
 using Frollicle.Core;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PieceGenerator : MonoBehaviour
 {
     //private int _rotation; //TODO
     public GameObject PlayerCube;
     public GameObject PlantedHair;
+    [SerializeField] private float _moveSpeed = 15f;
+    private Vector2 _inputVector;
+    private Rigidbody playerRb;
     private Piece CurrentPiece;
     private Piece[] RandomPieces = new Piece[] { Piece.I, Piece.O, Piece.T, Piece.S, Piece.Z, Piece.J, Piece.L };
     private Dictionary<Piece, List<Vector3>> SquarePositions = new Dictionary<Piece, List<Vector3>>
@@ -20,8 +24,15 @@ public class PieceGenerator : MonoBehaviour
         { Piece.L, new List<Vector3> { new Vector3(0.5f, 0.5f, -0.1f), new Vector3(-0.5f, 0.5f, -0.1f), new Vector3(1.5f, 0.5f, -0.1f), new Vector3(1.5f, 1.5f, -0.1f) } }
     };
 
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+        Move();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (IsBlocked())
@@ -61,6 +72,17 @@ public class PieceGenerator : MonoBehaviour
                 }
             }
         }
+    }
+    
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        _inputVector = context.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 moveVector3 = new Vector3(_inputVector.x, _inputVector.y, 0);
+        playerRb.velocity = moveVector3 * _moveSpeed;
     }
 
     private bool IsBlocked()
