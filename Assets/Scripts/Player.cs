9cce7fd4e00b5e7c5290301f4d1fa9945bd2a42d
hashbert <obert.chen@gmail.com>
2023-02-04 00:25:15 -0500

    some changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed = 7f;
+    private Vector2 _inputVector;
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        _inputVector = context.ReadValue<Vector2>();
+    }
+    private void Move()
+    {
+        Debug.Log(_inputVector);
+        playerRb.velocity = _inputVector * _moveSpeed;
     }
 }
