58b5b74e2090ba6b6fe4926918c08c17f05ea7f5
hashbert <obert.chen@gmail.com>
2023-02-04 18:31:31 -0500

    timer implemented

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _textTimer;
+    [SerializeField] private float _timer = 120f;
+    [SerializeField] private float _waitTime = 3f;
+    [SerializeField] private Animator _anim;
+    private bool _isTimerOn = false;
+
+    public static event Action OnTimeUp;
+
+    private void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    private void Update()
+    {
+        if (_isTimerOn)
+        {
+            _timer -= Time.deltaTime;
+            DisplayTime();
+            if (_timer <= 1)
+            {
+                _isTimerOn = false;
+                OnTimeUp?.Invoke();
+                StopTimer();
+                ResetTimer();
+                _anim.SetBool("TimeEnd", true);
+                //Debug.Log(_anim.GetFloat("Direction"));
+                //_anim.Play("TimerChange", -1, float.NegativeInfinity);
+            }
+        }
+    }
+
+    private void ResetTimeOnSpeed01(Scene oldScene, Scene newScene)
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Speed01"))
+        {
+            ResetTimer();
+            StartTimer();
+            _anim.Play("ResetPosition", -1, 0f);
+        }
+    }
+    private void ShowFinalSpeedScore(Scene oldScene, Scene newScene)
+    {
+        if (SceneManager.GetActiveScene().name.Equals("SpeedRun"))
+        {
+            StopTimer();
+            _anim.Play("ShowFinalScore", -1, 0f);
+        }
+    }
+
+    private void DisplayTime()
+    {
+        int minutes = Mathf.FloorToInt(_timer / 60.0f);
+        int seconds = Mathf.FloorToInt(_timer - minutes * 60);
+        _textTimer.text = string.Format("Time Left: " + "{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(_waitTime);
+        _anim.enabled = true;
+        _isTimerOn = true;
+    }
+
+    private void StopTimer()
+    {
+        _isTimerOn = false;
+    }
+    private void ResetTimer()
+    {
+        _timer = 120f;
+    }
+}
