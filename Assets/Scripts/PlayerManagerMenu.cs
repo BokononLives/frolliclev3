cbbe90c82eda3216baa366b61c727d1ea46ae69e
hashbert <obert.chen@gmail.com>
2023-02-04 21:30:35 -0500

    timer prefab added

---
+++ Assets/Scripts/PlayerManagerMenu.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManagerMenu : MonoBehaviour
+{
+    [SerializeField] private Material _hairMaterial;
+    private PlayerInputManager _playerInputManager;
+    private GameObject _playerPrefab;
+    public event Action<PlayerInput> OnPlayerJoined;
+    private Color _playerHairColor;
+    private Dictionary<int, Color> _playerColorDictionary = new Dictionary<int, Color> { };
+    private void Awake()
+    {
+        _playerInputManager = GetComponent<PlayerInputManager>();
+        _playerPrefab = _playerInputManager.playerPrefab;
+        Color randomColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+        _hairMaterial.color = randomColor;
+        _playerPrefab.GetComponentInChildren<Renderer>().sharedMaterial = _hairMaterial;
+    }
+    private void OnEnable()
+    {
+        OnPlayerJoined += CreateRandomHairColor;
+    }
+    private void OnDisable()
+    {
+        OnPlayerJoined -= CreateRandomHairColor;
+    }
+    public void CreateRandomHairColor(PlayerInput obj)
+    {
+        Material playerJoinedHairMaterial = new Material(_hairMaterial);
+        Color randomColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+        _playerHairColor = randomColor;
+        playerJoinedHairMaterial.color = randomColor;
+        _playerPrefab.GetComponentInChildren<Renderer>().sharedMaterial = playerJoinedHairMaterial;
+    }
+}
